9cd4fb66e1937e143edcbb2813317d9c01fe3469
ali Sajadi <chargoonmobileintern2@outlook.com>
2020-09-23 20:24:18 +0330

    Initial Commit

---
+++ iTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTask
+//
+//  Created by Ali Sajadi on 9/23/20.
+//  Copyright © 2020 Ali Sajadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
